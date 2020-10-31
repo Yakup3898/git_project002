0499c16ff10b0da834a144cb27369355e6d90674
Yakup3898 <73172258+Yakup3898@users.noreply.github.com>
2020-11-01 02:15:34 +0600

    Update FirstClass.java
    the second commit from the remote main

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -8,7 +8,9 @@ public class FirstClass {
 		System.out.println("This is second commit from local master");
 		System.out.println("This is the third commit from local master");
 		System.out.println("This is the first commit from remote repo");
-		System.out.println("This is the fourth commit from local main");
+		
+		
+		System.out.println("This is the second commit from remote main");
 	}
 
 }
